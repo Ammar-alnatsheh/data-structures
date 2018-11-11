6fd873ac2cc543f925d5582eea2bbf47fcc531b9
Ammar <ammar.alnatsheh@gmail.com>
2018-11-10 21:58:32 -0800

    improve the tree code/ add parent and remove from parent func

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,13 +2,14 @@ var Tree = function(value) {
   //complexity time is O(1)
   var newTree = {};
   newTree.value = value;
-  
+
   // add the addChild and contain methods to tree
   //using function shared concept
   extend(newTree, treeMethods);
-  
+
   // your code here
   newTree.children = [];  // fix me
+  newTree.parent = null;
 
   return newTree;
 };
@@ -20,11 +21,34 @@ treeMethods.addChild = function(value) {
   // create a new node 'tree object'
   // assign the value for the new node
   // push the new node in the children array
-  
+  // assign this as a parent to the new node
+
   var newNode = Tree(value);
   this.children.push(newNode);
-  
-  
+  newNode.parent = this;
+
+};
+
+treeMethods.parent = function() {
+  //complexity time is O(1)
+  // return the parent value
+  return this.parent;
+
+};
+
+treeMethods.removeFromParent = function() {
+  //complexity time is O(1)
+  //disassociates the tree with its parent (in both directions)
+  if ( this.parent !== null ) {
+    // this tree is not the root
+    var sibling = this.parent.children;
+    var idx = sibling.indexOf(this);
+    sibling.splice(idx,1);
+  }
+
+  this.parent = null;
+  return this;
+
 };
 
 treeMethods.contains = function(target) {
@@ -34,11 +58,11 @@ treeMethods.contains = function(target) {
 
   if (this.value === target) {
     return true;
-    
+
   } else if (this.children.length === 0) {
     // node is a leaf
     return false;
-    
+
   } else {
     //iterate through children
     for (var i = 0; i < this.children.length; i++) {
@@ -49,7 +73,7 @@ treeMethods.contains = function(target) {
     }
     return false;
   }
-  
+
 };
 
 var extend = function(to, from) {
